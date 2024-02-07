87e489c61f79f47f89492a151698cf67092d7633
enibalo <abibalo@hotmail.com>
2024-02-07 00:05:16 -0700

    Enable book title selection

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
+
 const book_image = document.querySelector("img.ResponsiveImage");
 
 book_image.style.border = "2px solid red";
+
+const book_title = document.querySelector(".Text.Text__title1");
+
+let text = book_title.style.text;
+
 
