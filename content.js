e0868acd36fd79d19f402d32adb7b0a847f2116e
enibalo <abibalo@hotmail.com>
2024-02-09 01:05:42 -0700

    Fetch overdrive from internet

--- content.js
+++ content.js
@@ -1,10 +1,31 @@
 
+
+
+fetch("https://calgary.overdrive.com/search?query=A+Bigger+Prize+margaret+heffernan") 
+    .then(function(response) {
+       
+        return response.text();
+
+    })
+    .then( function(html){
+        
+        let parser = new DOMParser();
+       
+        const doc = parser.parseFromString(html, "text/html");
+        const loan_status = doc.querySelector("section#search");
+        console.log(loan_status);
+        return 1;
+    });
+
 const book_image = document.querySelector("img.ResponsiveImage");
 
 book_image.style.border = "2px solid red";
 
 const book_title = document.querySelector(".Text.Text__title1");
 
-let text = book_title.style.text;
+book_title.style.color = "red";
+
+let text = book_title.textContent;
+
 
 
