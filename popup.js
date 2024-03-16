62b61b7363b6f32b5e72c8361495d99eef2d536e
enibalo <abibalo@hotmail.com>
2024-03-16 13:38:21 -0600

    Make wait times more specific

--- popup.js
+++ popup.js
@@ -1,10 +1,56 @@
+function getWaitTime(wait){
+    let result = "Wait time: ";
+    let years = Math.floor(wait/365);
+    wait = wait - (years * 365); 
+    let months = Math.floor(wait/30); 
+    wait = wait - (months * 30);
+    let weeks = Math.floor(wait / 7);
+    wait = wait - (weeks * 7);
+    let days = wait; 
+
+    if (years > 0){
+        if ( years == 1){
+            result += years + " year ";
+        }else{
+            result += years + " years ";
+        }
+
+    }
+
+    if (months > 0){
+        if ( months == 1 ){
+            result += months + " month ";
+        }else{
+            result += months + " months ";
+        }
+    }
+
+    if (weeks > 0){
+        if (weeks == 1){
+            result += weeks + " week ";
+        }else{
+            result += weeks + " weeks ";
+        }
+    }
+
+    if (days > 0){
+        if (days == 1){
+            result += days + " day"; 
+        }else{
+            result += days + " days";
+        }
+    }
+
+    return result;
+}
+
 function addTextMessage(message ){
     if (message == "") return;
-    let  text = "Title:" + message.title + "<br />";
-    text += "Author:" + message.author + "<br />";
+    let  text = "Title: " + message.title + "<br />";
+    text += "Author: " + message.author + "<br />";
     for (let i = 0; i < message.wait.length; i ++){
         text += "---------- <br />"
-        text += "Wait time: " + message.wait[i].wait + " days <br />";
+        text += getWaitTime(message.wait[i].wait) + "<br />";
         text += "Type: " + message.wait[i].type + "<br />";
     }
     
